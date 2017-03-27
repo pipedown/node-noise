de5210e58377fe873bbcaa0c6047d5549486ab3a
Damien Katz <damienkatz@Kayos.local>
2017-03-27 10:17:02 -0700

    add dependency on co and tests that use it

--- test/index.js
+++ test/index.js
@@ -31,4 +31,37 @@ exports['test basic'] = function(assert, done) {
     });
 }
  
+var co = require('co');
+
+exports['test yield'] = function(assert, done) {
+    co(function*() {
+        // open an index. `true` means if it doesn't exist, create one.
+        var index = yield noise.open("first_index", true);
+        assert.ok(index, "index opened/created");
+
+        // Insert multiple documents
+        var r = yield index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]);
+        assert.equal(2, r.length, "added 2 documents");
+
+        // find one of the documents
+        var r = yield index.query('find {foo: =="bar"}');    
+        assert.deepEqual(r, ["a"], "doc \"a\" found");
+        
+        // delete that document
+        var r = yield index.delete(r);
+        assert.deepEqual(r, [true], "doc \"a\" deleted");
+
+        // Close connection
+        yield index.close();
+        assert.ok(true, "index closed");
+
+        // `drop` the index, completely removing all data from disk.
+        yield noise.drop("first_index");
+        assert.ok(true, "index dropped");
+
+    }).catch(function(err) {
+        console.log(err.stack);
+    });
+};
+
 if (module == require.main) require('test').run(exports)
