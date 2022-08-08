fc8d719085c18dc4a2aaee9f166d4a9cb742ed96
Volker Mische <volker.mische@gmail.com>
2022-08-08 20:19:54 +0200

    fix: update JavaScript dependencies
    Some dependencies are no longer needed, the rest was updated to
    their latest version with (intentionally) minimal changes. The
    code will be updated to more modern JS at a later stage.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
+var assert = require('node:assert');
+var test = require('test');
+
 var noise = require('../lib/noise.js');
 
 // if test function expects second named argument it will be executed 
 // in async mode and test will be complete only after callback is called 
-exports['test basic'] = function(assert, done) {
+test('test basic', function(t, done) {
     var index = noise.open("tmp/firstrealtest", true);
     index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
         assert.deepEqual(resp, ["a","b"], "docs created");
@@ -24,9 +27,9 @@ exports['test basic'] = function(assert, done) {
     }).catch(error => {
         console.log(error);
     });
-}
+});
 
-exports['test iterable'] = function(assert, done) {
+test('test iterable', function(t, done) {
     var index = noise.open("tmp/iterabletesttest", true);
     index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
         assert.deepEqual(resp, ["a","b"], "docs created");
@@ -37,13 +40,15 @@ exports['test iterable'] = function(assert, done) {
             ids.push(value);
         }
         assert.deepEqual(ids, ["a", "b"], "doc a and b found");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-}
+});
 
-exports['test params'] = function(assert, done) {
+test('test params', function(t, done) {
     var index = noise.open("tmp/paramstest", true);
     index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
         assert.deepEqual(resp, ["a","b"], "docs created");
@@ -54,13 +59,15 @@ exports['test params'] = function(assert, done) {
             ids.push(value);
         }
         assert.deepEqual(ids, ["a", "b"], "doc a and b found");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-}
+});
 
-exports['test bad open'] = function(assert, done) {
+test('test bad open', function(t, done) {
     var index = noise.open("", true);
     index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
         console.log(resp);
@@ -69,9 +76,9 @@ exports['test bad open'] = function(assert, done) {
         assert.ok(true, "expected: " + error);
         done();
     });
-}
+});
 
-exports['test bad query'] = function(assert, done) {
+test('test bad query', function(t, done) {
     var index = noise.open("tmp/badquery", true);
     index.query('find {foo: =="bar"').then(resp => {
         console.log(resp);
@@ -86,9 +93,9 @@ exports['test bad query'] = function(assert, done) {
             assert.ok(false, "unexpected error: " + error);
         })
     });
-}
+});
 
-exports['test multi concurrent add'] = function(assert, done) {
+test('test multi concurrent add', function(t, done) {
     var index = noise.open("tmp/multiadd", true);
     index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
         assert.deepEqual(resp, ["a", "b"], "added a and b");
@@ -116,9 +123,9 @@ exports['test multi concurrent add'] = function(assert, done) {
     }).catch(error => {
         assert.ok(false, "failed" + error);
     });
-};
+});
 
-exports['test multi instances opened'] = function(assert, done) {
+test('test multi instances opened', function(t, done) {
     var index1 = noise.open("tmp/multiinst", true);
     var index2;
     index1.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
@@ -152,9 +159,9 @@ exports['test multi instances opened'] = function(assert, done) {
     }).catch(error => {
         assert.ok(false, "failed " + error);
     });
-};
+});
 
-exports['test completely empty document'] = function(assert, done) {
+test('test completely empty document', function(t, done) {
     var index = noise.open("tmp/emptydocument", true);
     var doc = {};
     var id;
@@ -165,13 +172,15 @@ exports['test completely empty document'] = function(assert, done) {
     }).then(iter => {
         assert.deepEqual(iter.next().value, {_id: id}, "Empty document is possible");
         assert.equal(iter.next().done, true, "done");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-};
+});
 
-exports['test document without _id'] = function(assert, done) {
+test('test document without _id', function(t, done) {
     var index = noise.open("tmp/withoutid", true);
     var doc = {foo: 'bar'};
     var id;
@@ -183,13 +192,15 @@ exports['test document without _id'] = function(assert, done) {
         assert.deepEqual(iter.next().value, {_id: id, foo: "bar"},
                          "Document without _id is possible");
         assert.equal(iter.next().done, true, "done");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-};
+});
 
-exports['test document with _id only'] = function(assert, done) {
+test('test document with _id only', function(t, done) {
     var index = noise.open("tmp/idonly", true);
     var doc = {_id: "a"};
     index.add([doc]).then(resp => {
@@ -199,13 +210,15 @@ exports['test document with _id only'] = function(assert, done) {
         assert.deepEqual(iter.next().value, {_id: "a"},
                          "Document with _id only is returned correctly");
         assert.equal(iter.next().done, true, "done");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-};
+});
 
-exports['test empty result'] = function(assert, done) {
+test('test empty result', function(t, done) {
     var index = noise.open("tmp/emptyresulttest", true);
     index.query('find {_cannotbefound: ==true}').then(iter => {
         assert.equal(iter.next().value,
@@ -220,13 +233,15 @@ exports['test empty result'] = function(assert, done) {
         assert.equal(iter.next().done,
                      true,
                      "Second `next()` call: .done is `true`");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-}
+});
 
-exports['test readme returns'] = function(assert, done) {
+test('test readme returns', function(t, done) {
     var index = noise.open("tmp/readmereturns", true);
     var doc = {
         "_id": "example",
@@ -293,13 +308,15 @@ exports['test readme returns'] = function(assert, done) {
         assert.deepEqual(Array.from(iter),
                          [[5463, 73]],
                          "return .faz[].biz is correct");
+        return index.close();
+    }).then(() => {
         done()
     }).catch(error => {
         console.log(error);
     });
-};
+});
 
-exports['test readme bind variables'] = function(assert, done) {
+test('test readme bind variables', function(t, done) {
     var index = noise.open("tmp/readmebind", true);
     var doc = {
         _id: "a",
@@ -339,13 +356,15 @@ exports['test readme bind variables'] = function(assert, done) {
         assert.deepEqual(Array.from(iter),
                          [{"x": [7, 9]}],
                          "combined bind is correct");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
     });
-};
+});
 
-exports['test iter unref'] = function(assert, done) {
+test('test iter unref', function(t, done) {
     var index = noise.open("tmp/iter_unref", true);
     index.add([{_id:"a",foo:"bar"}, {_id:"b", foo:"baz"}]).then(resp => {
         assert.equal(resp.length, 2, "docs created");
@@ -358,14 +377,16 @@ exports['test iter unref'] = function(assert, done) {
         assert.equal(iter.next().done, false, "first doc");
         assert.equal(iter.next().done, false, "second doc");
         assert.equal(iter.next().done, true, "done");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
         assert.ok(false, "should be no error");
     });
-};
+});
 
-exports['test geojson'] = function(assert, done) {
+test('test geojson', function(t, done) {
     var index = noise.open("tmp/geojson", true);
     index.add([{_id:"point","geometry":{"type":"Point","coordinates":[10.9,48.4]}},
       {"_id":"linestring","geometry":{"type":"LineString","coordinates":[[102.0,0.0],[103.0,1.0],[104.0,0.0],[105.0,1.0]]}}
@@ -381,12 +402,11 @@ exports['test geojson'] = function(assert, done) {
         assert.deepEqual(Array.from(iter),
                          ["point"],
                          "querying a subset works");
+        return index.close();
+    }).then(() => {
         done();
     }).catch(error => {
         console.log(error);
         assert.ok(false, "should be no error");
     });
-};
-
-
-if (module == require.main) require('test').run(exports)
+});
